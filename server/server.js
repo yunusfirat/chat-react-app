ef21ec2c20b4a6d2bbc17136046c5af72d15fa22
yunusfirat <yunusfirat44@gmail.com>
2021-05-28 19:17:14 +0100

    delete method has been done

--- server/server.js
+++ server/server.js
@@ -4,14 +4,25 @@ import Data from "./data.js";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.use("/list", (req, res) => {
-    if (Data.length >= 0) {
-        res.status(200).json(Data.map((element) => element.name));
-    } else {
-        res.status(404);
+app.delete("/lists/:name", function (req, res) {
+    let temp;
+    for (let i = 0; i < Data.length; i++){
+      if (Data[i].name === req.params.name){
+        temp = Data[i];
+        Data.splice(i,1);
+      }
     }
-});
-app.use("/list/:name", (req, res) => {
+    if (temp === undefined){
+      console.log(temp);
+      res.status(404);
+      res.send(`The item ${req.params.name} is not exist`);
+    }else {
+      res.status(200);
+      res.send(`The item ${req.params.name} has been deleted`);
+    }
+  });
+
+app.use("/lists/:name", (req, res) => {
     const found = Data.some((element) => element.name === req.params.name);
     if (found) {
         res.json(Data.filter((element) => element.name === req.params.name));
@@ -19,5 +30,13 @@ app.use("/list/:name", (req, res) => {
         res.status(400).json({ msg: `No member with the name of ${req.params.name}` });
     }
 });
+app.use("/lists", (req, res) => {
+    if (Data.length >= 0) {
+        res.status(200).json(Data.map((element) => element.name));
+    } else {
+        res.status(404);
+    }
+});
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
